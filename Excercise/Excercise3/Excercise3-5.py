be460af727a0e21845c989b1500ee7c3ec1640f8
ajitksagar <ajitnhacks@gmail.com>
2016-06-05 21:08:31 +0530

    Python Excercises from Think Python Orielly. Excercise 3.

--- Excercise/Excercise3/Excercise3-5.py
+++ Excercise/Excercise3/Excercise3-5.py
@@ -1,13 +1,48 @@
 def draw_grid(n):
-  h = n+1
-  v = n + 1
+    for i in range(n):
+        print_block(n)
+
+    print hor_block(n)
 
 
-def horizontal(h):
-    hor_str="+"
+def hor_block(n):
+    hor_str = "+"
+    extend_str = "----+"
     for i in range(4):
-       hor_str+='-'
+        hor_str += '-'
 
-    return hor_str+'+'
+    hor_str += '+'
+    j = 1
+    while j < n:
+        hor_str += extend_str
+        j += 1
 
-print horizontal(2)
+    return hor_str
+
+
+def ver_block(n):
+    ver_str = "|"
+    extend_str = "    |"
+    for i in range(4):
+        ver_str += ' '
+
+    ver_str += '|'
+
+    j = 1
+
+    while j < n:
+        ver_str += extend_str
+        j += 1
+
+    return ver_str
+
+
+def print_block(n):
+    print hor_block(n)
+    print ver_block(n)
+    print ver_block(n)
+    print ver_block(n)
+    print ver_block(n)
+
+
+draw_grid(6)
